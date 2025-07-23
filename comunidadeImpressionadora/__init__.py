c86bd702d6b548258d5e35751b6c67420f2eb95c
Lucas Amorim <lucas.amorim.porciuncula@gmail.com>
2025-07-23 13:16:31 -0300

    Adiciona pastas static, templates, instance e remove forms.py do rastreamento

--- comunidadeImpressionadora/__init__.py
+++ comunidadeImpressionadora/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '4b1e93d3e5b68b9aeb014d0f3b51592a'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Lucas/OneDrive/site python/instance/comunidade.db'
+
 
 database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'alert-info'
 
 from comunidadeImpressionadora import routes
 
--- comunidadeImpressionadora/forms.py
+++ comunidadeImpressionadora/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from comunidadeImpressionadora.models import Usuario
+from flask_login import current_user
 
 class FormCriarConta(FlaskForm):
     username = StringField('Nome do Usuário', validators=[DataRequired()])
@@ -9,8 +12,44 @@ class FormCriarConta(FlaskForm):
     confirmacao_senha = PasswordField('Confirmar senha', validators=[DataRequired(), EqualTo('senha')])
     botao_submit_criarconta = SubmitField('Criar Conta')
 
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            raise ValidationError('E-mail já cadastrado. Cadastre-se com outro e-mail ou faça login para continuar')
+    
+    def validate_username(self, username):
+        username = Usuario.query.filter_by(username=username.data).first()
+        if username:
+            raise ValidationError('Usuário já existente, tente outro nome ')
+
+
 class FormLogin(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 11)])
     lembrar_dados = BooleanField('Lembrar dados de Acesso')
     botao_submit_login = SubmitField('Login')
+
+
+class FormEditarPerfil(FlaskForm):
+    username = StringField('Nome do Usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    foto_perfil = FileField('Atualizar Foto de Perfil', validators=[FileAllowed(['jpg', 'png','JPEG'])])
+    
+    # Cursos
+    curso_excel = BooleanField('Excel Impressionador')
+    curso_vba = BooleanField('VBA Impressionador')
+    curso_powerbi = BooleanField('Power BI Impressionador')
+    curso_python = BooleanField('Python Impressionador')
+    curso_sql = BooleanField('SQL Impressionador')
+    curso_html_css_js = BooleanField('HTML, CSS e JavaScript Impressionador')
+    curso_ciencia_dados = BooleanField('Ciência de Dados Impressionadora')
+    
+    # Botão de submit
+    botao_submit_editarperfil = SubmitField('Salvar Alterações')
+    
+    def validate_email(self, email):
+        if current_user.email != email.data:
+            usuario = Usuario.query.filter_by(email=email.data).first()
+            if usuario:
+                raise ValidationError('E-mail já cadastrado. Cadastre-se com outro e-mail ou faça login para continuar.')
+
