768f6e01976b2cc451b0e66e817e5a153b14209d
Lucas Amorim <158764384+Lucas-1234567890@users.noreply.github.com>
2025-07-24 13:55:41 -0300

    Update __init__.py

--- comunidadeImpressionadora/__init__.py
+++ comunidadeImpressionadora/__init__.py
@@ -7,15 +7,11 @@ import os
 app = Flask(__name__, instance_relative_config=True)
 
 app.config['SECRET_KEY'] = '4b1e93d3e5b68b9aeb014d0f3b51592a'
-# Caminho relativo para a pasta instance, vai criar o arquivo dentro da pasta instance
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.dirname(__file__), 'comunidade.db')
-
-
-# Cria a pasta instance se não existir
-try:
-    os.makedirs(app.instance_path)
-except OSError:
-    pass
+if os.getenv('DATABASE_URL'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+else:
+    # Caminho relativo para a pasta instance, vai criar o arquivo dentro da pasta instance
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.dirname(__file__), 'comunidade.db')
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
